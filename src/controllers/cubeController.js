138bbdb09329f4ae2cb2f41c0a896d3e3add9227
mihalkoff <ushuaya@abv.bg>
2023-01-29 20:59:47 +0200

    cubeController getDetails update

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,4 +1,5 @@
 const Cube = require('../models/Cube');
+const db = require('../db.json');
 
 exports.getCreateCube = (req, res) => {
     res.render('create');
@@ -6,10 +7,26 @@ exports.getCreateCube = (req, res) => {
 
 exports.postCreateCube = (req, res) => {
     //save cube
-    const {name, description, imageUrl, difficultyLevel} = req.body;
+    const { name, description, imageUrl, difficultyLevel } = req.body;
     let cube = new Cube(name, description, imageUrl, difficultyLevel);
     Cube.save(cube);
 
     //redirect
     res.redirect('/');
+};
+
+exports.getDetails = (req, res) => {
+    let cubeId = Number(req.params.cubeId);
+
+    if(!cubeId) {
+        return res.redirect('404');
+    }
+
+    let cube = db.cubes.find(x => x.id === cubeId);
+
+    if(!cube) {
+        return res.redirect('404');
+    }
+
+    res.render('details', { cube });
 };
