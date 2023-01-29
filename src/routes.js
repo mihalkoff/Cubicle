a3b00d29f0ad2787c8203d60a827659bf7584319
mihalkoff <ushuaya@abv.bg>
2023-01-29 14:53:45 +0200

    Add homeController

--- src/routes.js
+++ src/routes.js
@@ -4,19 +4,23 @@
 //modular router
 
 const router = require('express').Router();
+const homeController = require('./controllers/homeController');
 const cubeController = require('./controllers/cubeController');
 
-router.get('/', (req, res) => {
-    res.render('index');
-});
+// router.get('/', (req, res) => {
+//     res.render('index');
+// });
 
-router.get('/about', (req, res) => {
-    res.render('about');
-});
+// router.get('/about', (req, res) => {
+//     res.render('about');
+// });
 
 // app.get('/create', (req, res) => {
 //     res.render('create');
 // });
+
+router.get('/', homeController.getHomePage);
+router.get('/about', homeController.getAboutPage);
 
 router.get('/create', cubeController.getCreateCube);
 router.post('/create', cubeController.postCreateCube);
