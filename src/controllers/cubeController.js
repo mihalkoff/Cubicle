8f407f667fec5c308f7d3120276d9cd75eafbc4a
mihalkoff <ushuaya@abv.bg>
2023-02-02 21:55:55 +0200

    Save cube to db

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,3 +1,4 @@
+// const Cube = require('../models/Cube_old');
 const Cube = require('../models/Cube');
 const db = require('../db.json');
 
@@ -5,12 +6,12 @@ exports.getCreateCube = (req, res) => {
     res.render('create');
 };
 
-exports.postCreateCube = (req, res) => {
+exports.postCreateCube = async (req, res) => {
     //save cube
     const { name, description, imageUrl, difficultyLevel } = req.body;
-    let cube = new Cube(name, description, imageUrl, difficultyLevel);
+    let cube = new Cube({ name, description, imageUrl, difficultyLevel });
     // Cube.save(cube);
-    cube.save();
+    await cube.save();
 
     //redirect
     res.redirect('/');
