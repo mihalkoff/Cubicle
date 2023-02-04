09ee2a41cf29b621daba0a70630d2883b4bf41f4
mihalkoff <ushuaya@abv.bg>
2023-02-04 20:03:58 +0200

    Add create accessory

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -1,8 +1,20 @@
 const router = require('express').Router();
 
+const Accessory = require('../models/Accessory');
+
 //url: /accessory/create
 router.get('/create', (req, res) => {
     res.render('accessory/create');
+});
+
+router.post('/create', async (req, res) => {
+    const { name, description, imageUrl } = req.body;
+
+    // const accessory = new Accessory()
+
+    await Accessory.create({ name, description, imageUrl });
+
+    res.redirect('/');
 });
 
 
